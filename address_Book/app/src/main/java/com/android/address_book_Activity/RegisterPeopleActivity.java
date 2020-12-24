63401e09b49976d03c022e265c7e404093c75ac9
DESKTOP-RPFTG8R\admin <e994301s@gmail.com>
2020-12-24 16:31:31 +0900

    전화번호 입력 진행 중

--- address_Book/app/src/main/java/com/android/address_book_Activity/RegisterPeopleActivity.java
+++ address_Book/app/src/main/java/com/android/address_book_Activity/RegisterPeopleActivity.java
@@ -3,8 +3,12 @@ package com.android.address_book_Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.android.address_book.R;
+import com.navdrawer.SimpleSideDrawer;
 
 /*
 ===========================================================================================================================
@@ -22,9 +26,23 @@ import com.android.address_book.R;
 
 public class RegisterPeopleActivity extends AppCompatActivity {
 
+    Button add_view;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_people);
+        add_view = findViewById(R.id.addTelNoButton);
+
+        add_view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RegisterAddPhoneNumber n_layout = new RegisterAddPhoneNumber(getApplicationContext());
+                LinearLayout con = (LinearLayout)findViewById(R.id.add_PhoneNumber_layout);
+                con.addView(n_layout);
+                add_view.setVisibility(View.INVISIBLE);
+            }
+        });
     }
 }
