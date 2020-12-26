d9d6c42a246872e855e6fb301cf75d75ba0b7e44
young <775973@daum.net>
2020-12-26 22:31:18 +0900

    1226 logincheck YOUNG

--- address_Book/app/src/main/java/com/android/Task/CUDNetworkTask.java
+++ address_Book/app/src/main/java/com/android/Task/CUDNetworkTask.java
@@ -1,4 +1,128 @@
 package com.android.Task;
 
-public class CUDNetworkTask {
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class CUDNetworkTask extends AsyncTask<Integer, String, Object> {
+
+    final static String TAG = "NetworkTask";
+    Context context = null;
+    String mAddr = null;
+    ProgressDialog progressDialog = null;
+
+    public CUDNetworkTask(Context context, String mAddr) {
+        this.context = context;
+        this.mAddr = mAddr;
+        Log.v(TAG, "Start : " + mAddr);
+    }
+
+    @Override
+    protected void onPreExecute() {
+        Log.v(TAG, "onPreExecute()");
+        progressDialog = new ProgressDialog(context);
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progressDialog.setTitle("Create/Update/Delete");
+        progressDialog.setMessage("Ins ....");
+        progressDialog.show();
+
+    }
+
+    @Override
+    protected Object doInBackground(Integer... integers) {
+        Log.v(TAG, "doInBackground()");
+
+        ///////////////////////////////////////////////////////////////////////////////////////
+        // Date : 2020.12.24
+        //
+        // Description:
+        // - jsp를 실행후에 return 값을 Json으로 받는다.
+        //
+        ///////////////////////////////////////////////////////////////////////////////////////
+
+        StringBuffer stringBuffer = new StringBuffer();
+        InputStream inputStream = null;
+        InputStreamReader inputStreamReader = null;
+        BufferedReader bufferedReader = null;
+        String result = null;
+
+        try {
+            URL url = new URL(mAddr);
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            httpURLConnection.setConnectTimeout(10000);
+            if(httpURLConnection.getResponseCode() == HttpURLConnection.HTTP_OK){
+                inputStream = httpURLConnection.getInputStream();
+                inputStreamReader = new InputStreamReader(inputStream);
+                bufferedReader = new BufferedReader(inputStreamReader);
+
+                while (true){
+                    String strline = bufferedReader.readLine();
+                    if(strline == null) break;
+                    stringBuffer.append(strline + "\n");
+                }
+
+                result = parser(stringBuffer.toString());
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    @Override
+    protected void onProgressUpdate(String... values) {
+        Log.v(TAG, "onProgressUpdate()");
+        super.onProgressUpdate(values);
+    }
+
+    @Override
+    protected void onPostExecute(Object o) {
+        Log.v(TAG, "onPostExecute()");
+        super.onPostExecute(o);
+        progressDialog.dismiss();
+    }
+
+    @Override
+    protected void onCancelled() {
+        Log.v(TAG,"onCancelled()");
+        super.onCancelled();
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////
+    // Date : 2020.12.24
+    //
+    // Description:
+    // - jsp의 결과인 Json을 Parsing한다.
+    // - Json 결과값
+    //      {
+    //           "result" : "1"
+    //      }
+    ///////////////////////////////////////////////////////////////////////////////////////
+
+    private String parser(String s){
+        Log.v(TAG,"Parser()");
+        String returnValue = null;
+
+        try {
+            Log.v(TAG, s);
+
+            JSONObject jsonObject = new JSONObject(s);
+            returnValue = jsonObject.getString("result");
+            Log.v(TAG, returnValue);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return returnValue;
+    }
 }
--- address_Book/app/src/main/java/com/android/address_book_Activity/MainActivity.java
+++ address_Book/app/src/main/java/com/android/address_book_Activity/MainActivity.java
@@ -3,7 +3,13 @@ package com.android.address_book_Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.QuickContactBadge;
+import android.widget.Toast;
 
+import com.android.Task.CUDNetworkTask;
 import com.android.address_book.R;
 
 
@@ -22,10 +28,83 @@ import com.android.address_book.R;
 */
 
 public class MainActivity extends AppCompatActivity {
+    String urlAddr = null;
+    EditText loginId;
+    EditText loginPw;
+    Button loginBtn;
+    String id, pw ;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        urlAddr = "http://192.168.35.251:8080/test/logincheck.jsp?";
+
+        loginId = findViewById(R.id.login_id);
+        loginPw = findViewById(R.id.login_pw);
+        loginBtn = findViewById(R.id.login_btn);
+
+        loginBtn.setOnClickListener(onClickListener);
     }
-}
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            id = loginId.getText().toString();
+            pw = loginPw.getText().toString();
+
+            urlAddr = urlAddr + "id=" + id + "&pw=" + pw;
+
+            String result = connectUpdateData();
+
+            if(result.equals("1")){
+                Toast.makeText(MainActivity.this, "로그인 완료", Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(MainActivity.this, "아이디와 비밀번호를 확인하세요!", Toast.LENGTH_SHORT).show();
+            }
+            finish();
+
+
+
+
+
+
+        }
+    };
+
+    private String connectUpdateData(){
+
+        String result = null;
+        try {
+            CUDNetworkTask updnetworkTask = new CUDNetworkTask(MainActivity.this, urlAddr);
+            ///////////////////////////////////////////////////////////////////////////////////////
+            // Date : 2020.12.24
+            //
+            // Description:
+            // - 수정 결과 값을 받기 위해 Object로 return후에 String으로 변환 하여 사용
+            //
+            ///////////////////////////////////////////////////////////////////////////////////////
+
+            Object obj = updnetworkTask.execute().get();
+            result = (String) obj;
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+
+
+
+
+
+
+
+
+}//---------------------------
