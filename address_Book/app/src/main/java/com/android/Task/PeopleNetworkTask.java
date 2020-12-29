08d65e8586cbf9689c621f86fb91606861012fb1
mg6613 <mg6613@naver.com>
2020-12-29 14:49:31 +0900

    Ming 20/12/29 // 14:50 메인 리스트 구성

---
+++ address_Book/app/src/main/java/com/android/Task/PeopleNetworkTask.java
@@ -0,0 +1,140 @@
+package com.android.Task;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.android.address_book.People;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
+public class PeopleNetworkTask extends AsyncTask<Integer, String, Object> {
+    final static String TAG = "NetworkTask";
+    Context context = null;
+    String mAddr = null;
+    ProgressDialog progressDialog = null;
+    ArrayList<People> members;
+
+    public PeopleNetworkTask(Context context, String mAddr) {
+        this.context = context;
+        this.mAddr = mAddr;
+        this.members = new ArrayList<People>();
+        Log.v(TAG, "Start : "+ mAddr);
+    }
+
+    @Override
+    protected void onPreExecute() {
+        Log.v(TAG, "onPreExecute()");
+        progressDialog = new ProgressDialog(context);
+        progressDialog.setProgressStyle(progressDialog.STYLE_SPINNER);
+        progressDialog.setTitle("Data Fetch");
+        progressDialog.setMessage("Get...");
+        progressDialog.show();
+    }
+
+
+
+    @Override
+    protected Object doInBackground(Integer... integers) {
+        Log.v(TAG, "doInBackground()");
+
+        StringBuffer stringBuffer = new StringBuffer();
+
+        InputStream inputStream = null;
+        InputStreamReader inputStreamReader = null;
+
+        BufferedReader bufferedReader = null;
+        Log.v(TAG, "before try");
+        try{
+            Log.v(TAG, "after try");
+            URL url = new URL(mAddr);
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            httpURLConnection.setConnectTimeout(10000);
+            Log.v(TAG, "Accept : "+httpURLConnection.getResponseCode());
+            if(httpURLConnection.getResponseCode() == HttpURLConnection.HTTP_OK){
+                inputStream = httpURLConnection.getInputStream();
+                inputStreamReader = new InputStreamReader(inputStream);
+                bufferedReader = new BufferedReader(inputStreamReader);
+
+                while (true){
+                    String strline = bufferedReader.readLine();
+                    if(strline == null) break;
+                    stringBuffer.append(strline + "\n");
+
+                }
+                Log.v(TAG, "StringBuffer : "+stringBuffer.toString());
+                Log.v("here","parser11");
+                parser(stringBuffer.toString());
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            try{
+                if(bufferedReader != null) bufferedReader.close();
+                if(inputStreamReader != null) inputStreamReader.close();
+                if(inputStream != null) inputStream.close();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+        return members;
+    }
+
+    @Override
+    protected void onProgressUpdate(String... values) {
+        Log.v(TAG, "doProgressUpdate()");
+        super.onProgressUpdate(values);
+    }
+
+    @Override
+    protected void onPostExecute(Object o) {
+        Log.v(TAG, "doPostExecute()");
+        super.onPostExecute(o);
+        progressDialog.dismiss();
+    }
+
+    @Override
+    protected void onCancelled() {
+        Log.v(TAG, "onCancelled()");
+        super.onCancelled();
+    }
+
+    private void parser(String s){
+        Log.v(TAG, "parser()");
+        try {
+            JSONObject jsonObject = new JSONObject(s);
+            JSONArray jsonArray = new JSONArray(jsonObject.getString("people_info"));
+            Log.v(TAG, "parser() in");
+            members.clear();
+            for(int i = 0 ; i<jsonArray.length() ; i++){
+                JSONObject jsonObject1 = (JSONObject) jsonArray.get(i);
+                String no = jsonObject1.getString("no");
+                String name = jsonObject1.getString("name");
+                String tel = jsonObject1.getString("tel");
+                String email = jsonObject1.getString("email");
+                String relation = jsonObject1.getString("relation");
+                String memo = jsonObject1.getString("memo");
+                String image = jsonObject1.getString("image");
+
+                Log.v("here", "no"+no);
+
+                People people = new People(no, name, tel, email, relation, memo, image);
+                members.add(people);
+            }
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+}
