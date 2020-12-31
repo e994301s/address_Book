dda22f841ff95db289b8839b088b9bb5e99444bf
RiaSong <donna_s@sookmyung.ac.kr>
2020-12-31 18:43:53 +0900

    12/31 u_u

--- address_Book/app/src/main/java/com/android/address_book_Activity/FirstFragment.java
+++ address_Book/app/src/main/java/com/android/address_book_Activity/FirstFragment.java
@@ -66,8 +66,8 @@ public class FirstFragment extends Fragment {
         macIP = getArguments().getString("macIP");
 
         urlAddr = "http://" + macIP + ":8080/test/";
-        urlAddr1 = urlAddr + "people_query_all.jsp?email=" + email;
-        urlAddr2 = urlAddr + "group_query_all.jsp?email=" + email;
+        urlAddr1 = urlAddr + "people_query_all_no.jsp?email=" + email;
+        urlAddr2 = urlAddr + "group_query_all_no.jsp?email=" + email;
 
 
 
