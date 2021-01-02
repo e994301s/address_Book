531cbad0af062bccab9353ae5a0b6955f5554984
RiaSong <donna_s@sookmyung.ac.kr>
2021-01-02 17:24:10 +0900

    1/2 Ria_ Modify

--- address_Book/app/src/main/java/com/android/address_book_Activity/ModifyPeopleActivity.java
+++ address_Book/app/src/main/java/com/android/address_book_Activity/ModifyPeopleActivity.java
@@ -4,7 +4,12 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.webkit.WebChromeClient;
@@ -19,12 +24,27 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
+
 import com.android.Task.CUDNetworkTask;
 import com.android.Task.PeopleNetworkTask;
 import com.android.address_book.People;
 import com.android.address_book.R;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 /*
 ===========================================================================================================================
@@ -64,6 +84,19 @@ public class ModifyPeopleActivity extends Activity {
     String no, name, email, relation, memo, macIP;
     ArrayList<People> members;
     String urlImage;
+    int imageCheck=0;
+
+    // 사진 올리고 내리기 위한 변수들
+    private final int REQ_CODE_SELECT_IMAGE = 100;
+    private String img_path = new String();
+    private Bitmap image_bitmap_copy = null;
+    private Bitmap image_bitmap = null;
+    String imageName = null;
+    private String f_ext = null;
+    File tempSelectFile;
+
+    String url;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -94,6 +127,17 @@ public class ModifyPeopleActivity extends Activity {
         phoneno = members.get(0).getPhoneno();
 
 
+        // 사진 연결
+        url = "http://"+macIP+":8080/test/multipartRequest.jsp";
+
+        tv_editPeopleImage = findViewById(R.id.tv_editPeopleImage);
+        editImage.setOnClickListener(onClickListener);
+        tv_editPeopleImage.setOnClickListener(onClickListener);
+
+        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                .permitDiskReads()
+                .permitDiskWrites()
+                .permitNetwork().build());
 
 
 
@@ -171,13 +215,27 @@ public class ModifyPeopleActivity extends Activity {
 //                    startActivity(intent); //이동시킨 화면 시작
                     break;
                 case R.id.btn_updatePeople: // update
-                    peoplename=editName.getText().toString();
-                    peopleemail=editEmail.getText().toString();
-                    peoplememo=editMemo.getText().toString();
-                    phonetel.set(0, editPhone.getText().toString());
+                    // 순서 1. 네트워크 연결 및 이미지 서버에 전송, 이미지 이름 저장
+                    if(imageCheck==1){
+                        new Thread(new Runnable() {
+                            @Override
+                            public void run() {
+                                doMultiPartRequest();
+                            }
+                        }).start();
+                    }
+                        Log.v(TAG, "image Name : "+ imageName);
 
+                        // DB와 연결(NetworkTask)해서 정보 update
                     updatePeople(peopleno, peoplename, peopleemail, peoplerelation, peoplememo, peopleimage, phoneno, phonetel);
+
+
+
+
+                    //updatePeople(peopleno, peoplename, peopleemail, peoplerelation, peoplememo, peopleimage, phoneno, phonetel);
                     break;
+
+
                 case R.id.btn_remove: // delete
                              new AlertDialog.Builder(ModifyPeopleActivity.this)
                             .setIcon(R.drawable.ic_launcher_background)
@@ -189,6 +247,12 @@ public class ModifyPeopleActivity extends Activity {
 //                    urlAddr = "http://" + IP + ":8080/address/people_query_Delete.jsp?";
 //                    urlAddr = urlAddr + "no=" + peopleno+ "&phoneno" + phoneno;
 //                    connectDeleteData(urlAddr);
+                    break;
+                case R.id.tv_editPeopleImage:
+                    intent = new Intent(Intent.ACTION_PICK);
+                    intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+                    intent.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(intent, REQ_CODE_SELECT_IMAGE);
                     break;
             }
         }
@@ -320,4 +384,88 @@ public class ModifyPeopleActivity extends Activity {
         }
         return members;
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == REQ_CODE_SELECT_IMAGE) {
+            if (resultCode == Activity.RESULT_OK) {
+                try {
+                    imageCheck=1;
+                    img_path = getImagePathToUri(data.getData()); //이미지의 URI를 얻어 경로값으로 반환.
+                    Toast.makeText(getBaseContext(), "img_path : " + img_path, Toast.LENGTH_SHORT).show();
+                    Log.v("test", String.valueOf(data.getData()));
+                    //이미지를 비트맵형식으로 반환
+                    image_bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), data.getData());
+
+                    //image_bitmap 으로 받아온 이미지의 사이즈를 임의적으로 조절함. width: 400 , height: 300
+                    image_bitmap_copy = Bitmap.createScaledBitmap(image_bitmap, 400, 300, true);
+                    //editImage.setImageBitmap(image_bitmap_copy);
+
+                    // 파일 이름 및 경로 바꾸기(임시 저장)
+                    String date = new SimpleDateFormat("yyyyMMddHmsS").format(new Date());
+                    imageName = date+"."+f_ext;
+                    tempSelectFile = new File("/data/data/com.android.address_book/", imageName);
+                    OutputStream out = new FileOutputStream(tempSelectFile);
+                    image_bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+
+                    // 임시 파일 경로로 위의 img_path 재정의
+                    img_path = "/data/data/com.android.address_book/"+imageName;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    public String getImagePathToUri(Uri data) {
+        //사용자가 선택한 이미지의 정보를 받아옴
+        String[] proj = {MediaStore.Images.Media.DATA};
+        Cursor cursor = managedQuery(data, proj, null, null, null);
+        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+
+        //이미지의 경로 값
+        String imgPath = cursor.getString(column_index);
+        Log.d("test", imgPath);
+
+        //이미지의 이름 값
+        String imgName = imgPath.substring(imgPath.lastIndexOf("/") + 1);
+
+        // 확장자 명 저장
+        f_ext = imgPath.substring(imgPath.length()-3, imgPath.length());
+        Toast.makeText(ModifyPeopleActivity.this, "이미지 이름 : " + imgName, Toast.LENGTH_SHORT).show();
+//        this.imageName = imgName;
+
+        return imgPath;
+    }//end of getImagePathToUri()
+    //파일 변환
+    private void doMultiPartRequest() {
+
+        File f = new File(img_path);
+
+        DoActualRequest(f);
+    }
+
+    //서버 보내기
+    private void DoActualRequest(File file) {
+        OkHttpClient client = new OkHttpClient();
+
+        RequestBody body = new MultipartBody.Builder()
+                .setType(MultipartBody.FORM)
+                .addFormDataPart("image", file.getName(),
+                        RequestBody.create(MediaType.parse("image/*"), file))
+                .build();
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 } // end --------------------------------------------------------------------------------
